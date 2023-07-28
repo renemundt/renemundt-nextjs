3d3d8da11dfadb66b59504492cfe802ed1e0bf84
renemundt <renem@live.dk>
2023-07-28 16:43:24 +0200

    Initial commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function blog() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div>Blog</div>
+      </div>
+    </main>
+  )
+}
