1f9217cf69ed85ab489c74ee9600e8493bfd1f13
renemundt <renem@live.dk>
2023-07-28 18:13:27 +0200

    full width

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -14,14 +14,12 @@ const Post = ({ params }: Props) => {
   const post = getPostBySlug(params.slug, ['title', 'date', 'slug', 'author', 'content', 'ogImage', 'coverImage'])
   return (
     <main className="min-h-screen p-12">
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <article>
-          <h1>{post.title}</h1>
-          <time dateTime={post.date}>{format(new Date(post.date), 'LLLL	d, yyyy')}</time>
-          <ReactMarkdown>{post.content}</ReactMarkdown>
-          <section>{post.content}</section>
-        </article>
-      </div>
+      <article>
+        <h1>{post.title}</h1>
+        <time dateTime={post.date}>{format(new Date(post.date), 'LLLL	d, yyyy')}</time>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <section>{post.content}</section>
+      </article>
     </main>
   )
 }
