fda7d70e35a9579a87dd1e4f0639dd4fe2bdb762
renemundt <renem@live.dk>
2023-07-28 17:14:31 +0200

    list blog items

---
+++ app/_interfaces/post.ts
@@ -0,0 +1,14 @@
+import { Author } from './author'
+
+export interface BlogPost {
+  slug: string
+  title: string
+  date: string
+  coverImage: string
+  author: Author
+  excerpt: string
+  ogImage: {
+    url: string
+  }
+  content: string
+}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link'
+import { getAllPosts } from '../_lib/posts'
+
 export default function blog() {
+  const posts = getAllPosts(['title', 'date', 'slug', 'author', 'coverImage', 'excerpt'])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div>Blog</div>
+        {posts &&
+          posts.length > 0 &&
+          posts.map((post, i) => (
+            <>
+              <Link key={i} as={`/posts/${post.slug}`} href="/posts/[slug]">
+                {post.title}
+              </Link>
+              <br />
+            </>
+          ))}
       </div>
     </main>
   )
